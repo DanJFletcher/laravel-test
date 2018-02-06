26f088e862c2a19361e8f589d13abdcdadf5d2a5
DanJFletcher <dannyjfletcher@outlook.com>
2018-02-05 19:22:04 -0500

    Quick maths

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravels
+                    quick maths
                 </div>
 
                 <div class="links">
